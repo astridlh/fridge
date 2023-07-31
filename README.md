259692d1d2173cb1237bb6c847001116840b7730
astridlh <75378190+astridlh@users.noreply.github.com>
2023-07-31 20:40:31 +0200

    chore: add structs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fridge
  this is fridge
+
+
+Purpose of exisitence: 
+
+We want to create a digitial container for a fridge and/or storage unit. It should let one know when food is about to expire.
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+#[derive(Debug)]
+struct Date {
+    day: i8,
+    month: i8,
+    year: i32, // TODO: deal with leap years/dates
+}
+
+impl Date {
+    fn new() -> Self {
+        Self {
+            day: 0,
+            month: 0,
+            year: 0,
+        }
+    } // TODO: implement
+}
+
+#[derive(Debug)]
+struct Food {
+    name: String,
+    open: bool,
+    expriry_date_unopened: Date,
+    expiriy_date_opened: Date,
+}
+
+impl Food {
+    fn new(name: String, expriry_date_unopened: Date, expiriy_date_opened: Date) -> Self {
+        Self {
+            name,
+            open: false,
+            expriry_date_unopened,
+            expiriy_date_opened,
+        }
+    }
+
+    fn is_after_date(&self, date: Date) -> bool {
+        true // TODO: implement
+    }
+}
+
+fn main() {
+    let egg = Food::new(String::from("egg"), Date::new(), Date::new());
+
+    println!("{:?}", egg);
+}
